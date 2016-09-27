e5ad928e41c291887c1ce6da219a670d442337f2
hassan189 <hassan.abdul189@gmail.com>
2016-09-27 22:21:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/ new exp',function(req,res){
+    res.sendFile('I FOUND IT');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
