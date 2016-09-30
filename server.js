81e4316b7cefcffcfd4dd23b60c2208efffacbc2
hassan189 <hassan.abdul189@gmail.com>
2016-10-01 00:53:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/hash', function (req,res) {
-    res.send('cool');
+    res.sendFile(path.join(__dirname, 'ui', 'hash.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
